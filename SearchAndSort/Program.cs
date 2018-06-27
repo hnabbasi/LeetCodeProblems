576355dd680585ad5ae1079988ac641224055127
Hussain Abbasi <hnabbasi@outlook.com>
2018-06-27 01:02:13 -0500

    Added Trees. Invert Tree

---
+++ SearchAndSort/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SearchAndSort
+{
+    class MainClass
+    {
+        static Stopwatch _sw;
+        public static void Main(string[] args)
+        {
+            _sw = new Stopwatch();
+            BinarySearchProblem();
+        }
+
+        static void BinarySearchProblem() {
+            var searchArray = new int[] { 1, 5, 6, 8, 12, 16, 25, 39, 48, 55, 64, 71, 98 };
+            var search = 98;
+
+            Debug.WriteLine($"Search :{search}");
+            _sw.Start();
+            var index = BinarySearch(searchArray, search);
+            _sw.Stop();
+            Debug.WriteLine($"BinarySearch took {_sw.Elapsed.TotalMilliseconds}");
+            _sw.Reset();
+
+            // Slower - Interesting.
+            //_sw.Start();
+            //index = Array.BinarySearch(searchArray, search);
+            //_sw.Stop();
+            //Debug.WriteLine($"Array.BinarySearch took {_sw.Elapsed.TotalMilliseconds}");
+            //_sw.Reset();
+
+            if (index > -1)
+                Debug.WriteLine($"Found {searchArray[index]} at index {index}");
+            else
+                Debug.WriteLine("Not found.");
+        }
+
+        static int BinarySearch(int[] values, int val)
+        {
+            var first = 0;
+            var last = values.Length - 1;
+            var mid = 0;
+            var found = false;
+
+            while (first <= last)
+            {
+                mid = (first + last) / 2;
+
+                if(values[mid] == val) {
+                    found = true;
+                    break;
+                }
+
+                if (val > values[mid]) {
+                    first = mid + 1;
+                } else if (val < values[mid]) {
+                    last = mid - 1;
+                }
+            }
+
+            return found ? mid : -1;
+        }
+    }
+}
---
+++ TreesAndGraphs/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TreesAndGraphs
+{
+    class TreeNode {
+        public int Data;
+        public TreeNode Left;
+        public TreeNode Right;
+        public TreeNode(int data) { Data = data; }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //FindLaddersProblem();
+            InvertTreeProblem();
+        }
+
+        #region Invert Tree
+        private static void InvertTreeProblem()
+        {
+            var tree = new TreeNode(4)
+            {
+                Left = new TreeNode(2)
+                {
+                    Left = new TreeNode(1),
+                    Right = new TreeNode(3)
+                },
+                Right = new TreeNode(7)
+                {
+                    Left = new TreeNode(6),
+                    Right = new TreeNode(9)
+                }
+            };
+
+            PrintTree(tree);
+            Console.WriteLine("");
+            PrintTree(InvertTree(tree));
+        }
+
+        static TreeNode InvertTree(TreeNode root) {
+            if (root == null)
+                return null;
+
+            var right = InvertTree(root.Right);
+            var left = InvertTree(root.Left);
+
+            root.Left = right;
+            root.Right = left;
+            return root;
+        }
+
+        static void PrintTree(TreeNode tree) {
+            if (tree == null) 
+                return;
+            
+            Console.Write($"{tree.Data} ");
+            PrintTree(tree.Left);
+            PrintTree(tree.Right);
+        }
+        #endregion
+
+        #region Find Ladders
+        static void FindLaddersProblem() {
+            var input = new List<string>() { "hot", "dot", "dog", "lot", "log", "cog" };
+            Console.WriteLine($"Result:");
+
+            var result = FindLadders("hit", "cog", input);
+
+            foreach (var item in result)
+            {
+                foreach (var item2 in item)
+                {
+                    Console.Write($"{item2} -> ");
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        static IList<IList<string>> FindLadders(string beginWord, string endWord, IList<string> wordList) {
+
+            // Remove beginWord if exists
+            if(wordList.Contains(beginWord))
+                wordList.Remove(beginWord);
+
+            // Add endWord if doesn't exist
+            if(!wordList.Contains(endWord))
+                wordList.Add(endWord);
+
+            // Initialize ladder with beginWord as key
+            var laddersDict = new Dictionary<string, List<List<string>>>
+            {
+                { beginWord, new List<List<string>> { new List<string> { beginWord } } }
+            };
+
+            // Initialize To Visit queue
+            var toVisitQueue = new Queue<string>();
+            toVisitQueue.Enqueue(beginWord);
+
+            while (toVisitQueue.Count > 0)
+            {
+            }
+
+            return new List<IList<string>> { (System.Collections.Generic.List<string>)wordList };
+        }
+        #endregion
+
+    }
+}
